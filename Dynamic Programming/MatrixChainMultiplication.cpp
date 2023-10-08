78f9ec2731cf9bd8d0523c5c1b05b59064d5a3c0
Saishree Kouda <96722257+Saishreekouda@users.noreply.github.com>
2023-10-08 16:58:20 +0530

    Created MatrixChainMultiplication.cpp

---
+++ Dynamic Programming/MatrixChainMultiplication.cpp
@@ -0,0 +1,60 @@
+/*
+Time Complexity: O(N*N*N)
+
+Reason: There are N*N states and we explicitly run a loop inside the function which will run for N times, therefore at max ‘N*N*N’ new problems will be solved.
+
+Space Complexity: O(N*N) + O(N)
+
+Reason: We are using an auxiliary recursion stack space(O(N))and a 2D array ( O(N*N)).
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int f(vector<int>& arr, int i, int j, vector<vector<int>>& dp){
+    
+    // base condition
+    if(i == j)
+        return 0;
+        
+    if(dp[i][j]!=-1)
+        return dp[i][j];
+    
+    int mini = INT_MAX;
+    
+    // partioning loop
+    for(int k = i; k<= j-1; k++){
+        
+        int ans = f(arr,i,k,dp) + f(arr, k+1,j,dp) + arr[i-1]*arr[k]*arr[j];
+        
+        mini = min(mini,ans);
+        
+    }
+    
+    return mini;
+}
+
+
+int matrixMultiplication(vector<int>& arr, int N){
+    
+    vector<vector<int>> dp(N,vector<int>(N,-1));
+    
+    int i =1;
+    int j = N-1;
+    
+    
+    return f(arr,i,j,dp);
+    
+    
+}
+
+int main() {
+	
+	vector<int> arr = {10, 20, 30, 40, 50};
+	
+	int n = arr.size();
+	
+	cout<<"The minimum number of operations is "<<matrixMultiplication(arr,n);
+	
+	return 0;
+}
