191bbe18482547442b52aee436aad90551c88f5b
Saishree Kouda <96722257+Saishreekouda@users.noreply.github.com>
2023-10-08 17:54:27 +0530

    Create Palindrome-Partitioning.cpp

---
+++ Dynamic Programming/Palindrome-Partitioning.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to check if a substring is a palindrome.
+bool isPalindrome(int i, int j, string &s) {
+    while (i < j) {
+        if (s[i] != s[j]) return false;
+        i++;
+        j--;
+    }
+    return true;
+}
+
+// Recursive function to find the minimum number of partitions to make palindromes.
+int minPartitions(int i, int n, string &str, vector<int> &dp) {
+    // Base case: If we've reached the end of the string.
+    if (i == n) return 0;
+
+    if (dp[i] != -1) return dp[i];
+    int minCost = INT_MAX;
+    // Consider all possible substrings starting from the current index.
+    for (int j = i; j < n; j++) {
+        if (isPalindrome(i, j, str)) {
+            // If the substring is a palindrome, calculate the cost and minimize it.
+            int cost = 1 + minPartitions(j + 1, n, str, dp);
+            minCost = min(minCost, cost);
+        }
+    }
+    return dp[i] = minCost;
+}
+
+// Main function to find the minimum number of partitions for palindrome partitioning.
+int palindromePartitioning(string str) {
+    int n = str.size();
+    vector<int> dp(n, -1);
+    // Calling the recursive function and subtracting 1 as it counts partitions, not cuts.
+    return minPartitions(0, n, str, dp) - 1;
+}
+
+int main() {
+    string str = "BABABCBADCEDE";
+    int partitions = palindromePartitioning(str);
+    cout << "The minimum number of partitions: " << partitions << "\n";
+    return 0;
+}
