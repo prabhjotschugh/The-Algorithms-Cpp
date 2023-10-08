a7bc77294f1427c2536e17786a61980db97acf8c
Vikash Katiyar <89769715+Vikashkatiyar@users.noreply.github.com>
2023-10-08 11:28:53 +0530

    Create Postorder_Traversal.cpp
    Create Postorder_Traversal.cpp

---
+++ Trees/Postorder_Traversal.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class TreeNode {
+public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int value) : val(value), left(nullptr), right(nullptr) {}
+};
+
+void postorderTraversal(TreeNode* root) {
+    if (!root) return;
+    
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    cout << root->val << " ";
+}
+
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    cout << "postorder Traversal : ";
+    postorderTraversal(root);
+    cout << endl;
+
+    return 0;
+}
