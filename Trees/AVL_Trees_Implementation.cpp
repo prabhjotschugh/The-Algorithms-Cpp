4810e89144a086d4561fc54d98cac23314ecd0d7
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2023-11-09 21:04:29 +0530

    Create AVL_Trees_Implementation.cpp

---
+++ Trees/AVL_Trees_Implementation.cpp
@@ -0,0 +1,226 @@
+#include <iostream>
+#include <algorithm>
+
+class Node {
+public:
+    int key;
+    Node* left;
+    Node* right;
+    int height;
+    
+    Node(int k) : key(k), left(nullptr), right(nullptr), height(1) {}
+};
+
+class AVLTree {
+private:
+    Node* root;
+
+    int getHeight(Node* node) {
+        if (node == nullptr)
+            return 0;
+        return node->height;
+    }
+
+    int getBalanceFactor(Node* node) {
+        if (node == nullptr)
+            return 0;
+        return getHeight(node->left) - getHeight(node->right);
+    }
+
+    Node* rightRotate(Node* y) {
+        Node* x = y->left;
+        Node* T2 = x->right;
+
+        x->right = y;
+        y->left = T2;
+
+        y->height = std::max(getHeight(y->left), getHeight(y->right)) + 1;
+        x->height = std::max(getHeight(x->left), getHeight(x->right)) + 1;
+
+        return x;
+    }
+
+    Node* leftRotate(Node* x) {
+        Node* y = x->right;
+        Node* T2 = y->left;
+
+        y->left = x;
+        x->right = T2;
+#include <iostream>
+#include <algorithm>
+
+class Node {
+public:
+    int key;
+    Node* left;
+    Node* right;
+    int height;
+    
+    Node(int k) : key(k), left(nullptr), right(nullptr), height(1) {}
+};
+
+class AVLTree {
+private:
+    Node* root;
+
+    int getHeight(Node* node) {
+        if (node == nullptr)
+            return 0;
+        return node->height;
+    }
+
+    int getBalanceFactor(Node* node) {
+        if (node == nullptr)
+            return 0;
+        return getHeight(node->left) - getHeight(node->right);
+    }
+
+    Node* rightRotate(Node* y) {
+        Node* x = y->left;
+        Node* T2 = x->right;
+
+        x->right = y;
+        y->left = T2;
+
+        y->height = std::max(getHeight(y->left), getHeight(y->right)) + 1;
+        x->height = std::max(getHeight(x->left), getHeight(x->right)) + 1;
+
+        return x;
+    }
+
+    Node* leftRotate(Node* x) {
+        Node* y = x->right;
+        Node* T2 = y->left;
+
+        y->left = x;
+        x->right = T2;
+
+        x->height = std::max(getHeight(x->left), getHeight(x->right)) + 1;
+        y->height = std::max(getHeight(y->left), getHeight(y->right)) + 1;
+
+        return y;
+    }
+
+    Node* insert(Node* node, int key) {
+        if (node == nullptr)
+            return new Node(key);
+
+        if (key < node->key)
+            node->left = insert(node->left, key);
+        else if (key > node->key)
+            node->right = insert(node->right, key);
+        else
+            return node; // Duplicate keys not allowed
+
+        node->height = 1 + std::max(getHeight(node->left), getHeight(node->right));
+
+        int balance = getBalanceFactor(node);
+
+        // Left-Left case
+        if (balance > 1 && key < node->left->key)
+            return rightRotate(node);
+
+        // Right-Right case
+        if (balance < -1 && key > node->right->key)
+            return leftRotate(node);
+
+        // Left-Right case
+        if (balance > 1 && key > node->left->key) {
+            node->left = leftRotate(node->left);
+            return rightRotate(node);
+        }
+
+        // Right-Left case
+        if (balance < -1 && key < node->right->key) {
+            node->right = rightRotate(node->right);
+            return leftRotate(node);
+        }
+
+        return node;
+    }
+
+    void inorderTraversal(Node* node) {
+        if (node != nullptr) {
+            inorderTraversal(node->left);
+            std::cout << node->key << " ";
+            inorderTraversal(node->right);
+        }
+    }
+
+public:
+    AVLTree() : root(nullptr) {}
+
+    void insert(int key) {
+        root = insert(root, key);
+    }
+
+    void printInorder() {
+        inorderTraversal(root);
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    AVLTree avlTree;
+
+    avlTree.insert(10);
+    avlTree.insert(20);
+    avlTree.insert(30);
+
+    std::cout << "Inorder traversal of the AVL Tree: ";
+    avlTree.printInorder();
+
+    return 0;
+}
+￼Enter
+        x->height = std::max(getHeight(x->left), getHeight(x->right)) + 1;
+        y->height = std::max(getHeight(y->left), getHeight(y->right)) + 1;
+
+        return y;
+    }
+
+    Node* insert(Node* node, int key) {
+        if (node == nullptr)
+            return new Node(key);
+
+        if (key < node->key)
+            node->left = insert(node->left, key);
+        else if (key > node->key)
+            node->right = insert(node->right, key);
+        else
+            return node; // Duplicate keys not allowed
+
+        node->height = 1 + std::max(getHeight(node->left), getHeight(node->right));
+
+        int balance = getBalanceFactor(node);
+
+        // Left-Left case
+        if (balance > 1 && key < node->left->key)
+            return rightRotate(node);
+
+  // Right-Right case
+        if (balance < -1 && key > node->right->key)
+            return leftRotate(node);
+
+        // Left-Right case
+        if (balance > 1 && key > node->left->key) {
+            node->left = leftRotate(node->left);
+            return rightRotate(node);
+        }
+
+        // Right-Left case
+        if (balance < -1 && key < node->right->key) {
+            node->right = rightRotate(node->right);
+            return leftRotate(node);
+        }
+
+        return node;
+    }
+
+    void inorderTraversal(Node* node) {
+        if (node != nullptr) {
+            inorderTraversal(node->left);
+            std::cout << node->key << " ";
+            inorderTraversal(node->right);
+        }
+    }
